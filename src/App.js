c02af279b5de3fde01f2d26b39133f3b1d597072
JanAazar <aazarjan15003@gmail.com>
2025-05-12 23:51:09 -0600

    added navigator back

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import "./App.css";
 import HomeScreen from './HomeScreen';
 import ContactScreen from './ContactScreen';
+import Icon from './LockIcon';
 
 function App() {
   return (
     <Router>
-      {/* <div className="App"> */}
-        {/* <header className="navbar">
+      <div className="App">
+        <header className="navbar">
           <div className="navbar-logo">Safelock <Icon /></div>
           <nav className="navbar-links">
             <Link to="/">Home</Link>
@@ -17,16 +18,16 @@ function App() {
             <a href="#services">Services</a>
             <Link to="/contact">Contact Us</Link>
           </nav>
-        </header> */}
+        </header>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/contact" element={<ContactScreen />} />
         </Routes>
 
-        {/* <footer>
+        <footer>
           <p>&copy; Safelock Securities LLC. All rights reserved. </p>
         </footer>
-      </div> */}
+      </div>
     </Router>
   );
 }
